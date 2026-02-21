93c9dbf80aa2e03eb3e663e6808e453d2bfc733d
nasty8008 <nastya901296@gmail.com>
2026-02-21 19:24:03 +0400

    RCA-13: add extension function for mapping category dto

--- app/src/main/java/com/yourcompany/recipecomposeapp/ui/categories/model/CategoryUiModel.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/categories/model/CategoryUiModel.kt
@@ -1,6 +1,8 @@
 package com.yourcompany.recipecomposeapp.ui.categories.model
 
 import androidx.compose.runtime.Immutable
+import com.yourcompany.recipecomposeapp.Constants
+import com.yourcompany.recipecomposeapp.data.model.CategoryDto
 
 @Immutable
 data class CategoryUiModel(
@@ -8,4 +10,11 @@ data class CategoryUiModel(
     val title: String,
     val description: String,
     val imageUrl: String,
+)
+
+fun CategoryDto.toUiModel() = CategoryUiModel(
+    id = id,
+    title = title,
+    description = description,
+    imageUrl = if (imageUrl.startsWith("http")) imageUrl else Constants.ASSETS_URI_PREFIX + imageUrl
 )
