e4fd40a65cf1a1456e5de597beb4db18fca07fae
nasty8008 <nastya901296@gmail.com>
2026-02-28 16:38:18 +0400

    RCA-15: add Destination.kt

---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/Destination.kt
@@ -0,0 +1,9 @@
+package com.yourcompany.recipecomposeapp
+
+sealed class Destination(val route: String) {
+    object Categories : Destination("categories")
+    object Favorites : Destination("favorites")
+    object Recipes : Destination("recipes/{categoryId}") {
+        fun createRoute(categoryId: Int) = "recipes/$categoryId"
+    }
+}
