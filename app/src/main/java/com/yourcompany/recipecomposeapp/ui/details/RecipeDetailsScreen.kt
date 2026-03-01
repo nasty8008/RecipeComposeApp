e9cd2e396b500989443ae233e7fa20f694f5e5af
nasty8008 <nastya901296@gmail.com>
2026-03-02 00:18:00 +0400

    RCA-15: added RecipeDetailsScreen.kt

---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/details/RecipeDetailsScreen.kt
@@ -0,0 +1,21 @@
+package com.yourcompany.recipecomposeapp.ui.details
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.yourcompany.recipecomposeapp.ui.recipes.model.RecipeUiModel
+
+@Composable
+fun RecipeDetailsScreen(recipe: RecipeUiModel) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            recipe.title
+        )
+    }
+}
