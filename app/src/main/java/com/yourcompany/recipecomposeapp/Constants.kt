5e06314f2923285bba013cde161ef51949920cb9
nasty8008 <nastya901296@gmail.com>
2026-03-05 15:29:38 +0400

    RCA-19: fixed destination for recipe details

--- app/src/main/java/com/yourcompany/recipecomposeapp/Constants.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/Constants.kt
@@ -2,12 +2,12 @@ package com.yourcompany.recipecomposeapp
 
 object Constants {
     const val ASSETS_URI_PREFIX = "file:///android_asset/"
-    const val PARAM_RECIPE_ID = 0
-    const val KEY_RECIPE_OBJECT = "recipe/{${PARAM_RECIPE_ID}}"
+    const val KEY_RECIPE_OBJECT = "recipe"
+    const val PARAM_RECIPE_ID = "recipeId"
     const val DEEP_LINK_SCHEME = "recipeapp"
     const val DEEP_LINK_BASE_URL = "https://recipes.androidsprint.ru"
-}
 
-fun createRecipeDeepLink(recipeId: Int) {
-
+    fun createRecipeDeepLink(recipeId: Int): String {
+        return "$DEEP_LINK_BASE_URL/recipe/$recipeId"
+    }
 }
