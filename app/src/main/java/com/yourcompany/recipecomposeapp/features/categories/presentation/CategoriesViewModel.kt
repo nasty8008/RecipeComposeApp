ee6beaa0253963427080dfda380799951cb138a8
nasty8008 <nastya901296@gmail.com>
2026-03-18 13:52:33 +0400

    RCA-26: add data loading handling to CategoriesViewModel

--- app/src/main/java/com/yourcompany/recipecomposeapp/features/categories/presentation/CategoriesViewModel.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/features/categories/presentation/CategoriesViewModel.kt
@@ -2,10 +2,13 @@ package com.yourcompany.recipecomposeapp.features.categories.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yourcompany.recipecomposeapp.data.repository.getCategories
 import com.yourcompany.recipecomposeapp.features.categories.presentation.model.CategoriesUiState
+import com.yourcompany.recipecomposeapp.features.categories.presentation.model.toUiModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class CategoriesViewModel : ViewModel() {
@@ -14,7 +17,26 @@ class CategoriesViewModel : ViewModel() {
 
     init {
         viewModelScope.launch {
-            _uiState.value = CategoriesUiState()
+            _uiState.value = CategoriesUiState(isLoading = true, error = null)
+
+            try {
+                val categories = getCategories().map { it.toUiModel() }
+
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        categories = categories
+                    )
+                }
+
+            } catch (e: Exception) {
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        error = e.message ?: "Error"
+                    )
+                }
+            }
         }
     }
 }
