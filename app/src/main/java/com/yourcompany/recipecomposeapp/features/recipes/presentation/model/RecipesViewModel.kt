fb4e98bd9672edd2091aff47f5be204f26d077fd
nasty8008 <nastya901296@gmail.com>
2026-03-22 13:28:38 +0400

    RCA-27: change string literals to constants and decode categoryImageUrl

--- app/src/main/java/com/yourcompany/recipecomposeapp/features/recipes/presentation/model/RecipesViewModel.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/features/recipes/presentation/model/RecipesViewModel.kt
@@ -3,20 +3,22 @@ package com.yourcompany.recipecomposeapp.features.recipes.presentation.model
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yourcompany.recipecomposeapp.core.utils.Constants
 import com.yourcompany.recipecomposeapp.data.repository.getRecipesByCategoryId
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.net.URLDecoder
 
 class RecipesViewModel(state: SavedStateHandle) : ViewModel() {
     private val _uiState = MutableStateFlow(RecipesUiState())
     val uiState: StateFlow<RecipesUiState> = _uiState.asStateFlow()
 
-    private val categoryId: Int = state["categoryId"] ?: 0
-    private val categoryTitle: String = state["categoryTitle"] ?: ""
-    private val categoryImageUrl: String = state["categoryImageUrl"] ?: ""
+    private val categoryId: Int = state[Constants.PARAM_CATEGORY_ID] ?: 0
+    private val categoryTitle: String = state[Constants.PARAM_CATEGORY_TITLE] ?: ""
+    private val categoryImageUrl: String = URLDecoder.decode(state[Constants.PARAM_CATEGORY_IMAGE_URL] ?: "", "UTF-8")
 
     init {
         viewModelScope.launch {
