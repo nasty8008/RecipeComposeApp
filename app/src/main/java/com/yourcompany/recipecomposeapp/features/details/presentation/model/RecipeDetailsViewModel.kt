34fced53513a0fde7f55b923ff448566e502112f
nasty8008 <nastya901296@gmail.com>
2026-03-26 01:28:11 +0400

    RCA-29: update RecipeDetailsViewModel

--- app/src/main/java/com/yourcompany/recipecomposeapp/features/details/presentation/model/RecipeDetailsViewModel.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/features/details/presentation/model/RecipeDetailsViewModel.kt
@@ -1,10 +1,13 @@
 package com.yourcompany.recipecomposeapp.features.details.presentation.model
 
+import androidx.lifecycle.SavedStateHandle
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.yourcompany.recipecomposeapp.core.utils.Constants
 import com.yourcompany.recipecomposeapp.core.utils.FavoriteDataStoreManager
-import com.yourcompany.recipecomposeapp.features.recipes.presentation.model.RecipeUiModel
+import com.yourcompany.recipecomposeapp.data.repository.getRecipeById
+import com.yourcompany.recipecomposeapp.features.recipes.presentation.model.toUiModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,13 +15,39 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class RecipeDetailsViewModel(application: Application) : AndroidViewModel(application) {
+class RecipeDetailsViewModel(state: SavedStateHandle, application: Application) : AndroidViewModel(application) {
+
+    private val recipeId: Int = state[Constants.PARAM_RECIPE_ID] ?: 0
 
     private val favoriteManager = FavoriteDataStoreManager(application)
     private val _uiState = MutableStateFlow(RecipeDetailsUiState())
     val uiState: StateFlow<RecipeDetailsUiState> = _uiState.asStateFlow()
 
-    fun loadRecipe(recipe: RecipeUiModel) {
+    init {
+        loadRecipe(recipeId)
+    }
+
+//    fun loadRecipe(recipe: RecipeUiModel) {
+//        _uiState.update {
+//            it.copy(
+//                recipe = recipe,
+//                currentPortions = recipe.servings
+//            )
+//        }
+//
+//        viewModelScope.launch {
+//            favoriteManager.getFavoriteIdsFlow()
+//                .map { ids -> ids.contains(recipe.id.toString()) }
+//                .collect { isFavorite ->
+//                    _uiState.update {
+//                        it.copy(isFavorite = isFavorite)
+//                    }
+//                }
+//        }
+//    }
+
+    fun loadRecipe(recipeId: Int) {
+        val recipe = getRecipeById(recipeId)?.toUiModel() ?: return
         _uiState.update {
             it.copy(
                 recipe = recipe,
