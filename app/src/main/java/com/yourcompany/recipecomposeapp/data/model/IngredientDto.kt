5611a01ec736c9d9aa22d84edffc7f3daad5c369
nasty8008 <nastya901296@gmail.com>
2026-04-07 23:53:39 +0400

    RCA-33: replace URLConnection with OkHttp for categories get request

--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/IngredientDto.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/model/IngredientDto.kt
@@ -1,5 +1,8 @@
 package com.yourcompany.recipecomposeapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class IngredientDto(
     val quantity: Double,
     val unitOfMeasure: String,
--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/RecipeDto.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/model/RecipeDto.kt
@@ -1,5 +1,8 @@
 package com.yourcompany.recipecomposeapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RecipeDto(
     val id: Int,
     val title: String,
