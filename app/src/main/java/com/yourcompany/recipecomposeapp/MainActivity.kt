c7226f65ce43d6e01bbe364b530f75aa65591dca
nasty8008 <nastya901296@gmail.com>
2026-04-09 21:11:44 +0400

    RCA-34: replace OkHttp with Retrofit

--- app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
@@ -9,20 +9,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.yourcompany.recipecomposeapp.data.model.CategoryDto
-import com.yourcompany.recipecomposeapp.data.model.RecipeDto
+import androidx.lifecycle.lifecycleScope
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.yourcompany.recipecomposeapp.core.network.NetworkConfig
+import com.yourcompany.recipecomposeapp.core.network.api.RecipesApiService
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import kotlin.concurrent.thread
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 class MainActivity : ComponentActivity() {
     private var deepLinkIntent by mutableStateOf<Intent?>(null)
-    private val threadPool: ExecutorService = Executors.newFixedThreadPool(10)
-    private val client = OkHttpClient()
-    private val json = Json
+    private val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,41 +38,30 @@ class MainActivity : ComponentActivity() {
 
         Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
 
-        thread {
-            val request = Request.Builder()
-                .url("https://recipes.androidsprint.ru/api/category")
+        lifecycleScope.launch {
+            val contentType = "application/json".toMediaType()
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl(NetworkConfig.BASE_URL)
+                .addConverterFactory(json.asConverterFactory(contentType))
                 .build()
+            val service: RecipesApiService = retrofit.create(RecipesApiService::class.java)
+
 
             try {
-                client.newCall(request).execute().use { response ->
-                    Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-                    Log.i("!!!", "responseCode: ${response.code}")
-                    Log.i("!!!", "responseMessage: ${response.message}")
+                val categories = service.getCategories()
 
-                    val responseText: String = response.body.string()
-                    Log.i("!!!", responseText)
+                Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+                Log.i("!!!", "Количество категорий: ${categories.size}")
 
-                    val categories: List<CategoryDto> = json.decodeFromString(responseText)
-                    Log.i("!!!", "Количество категорий: ${categories.size}")
+                categories.forEach { category ->
+                    try {
+                        val recipes = service.getRecipesByCategory(category.id)
 
-                    categories.forEach { category ->
-                        threadPool.execute {
-                            val request = Request.Builder()
-                                .url("https://recipes.androidsprint.ru/api/category/${category.id}/recipes")
-                                .build()
-                            try {
-                                client.newCall(request).execute().use { response ->
-                                    val responseText: String = response.body.string()
-                                    val recipes: List<RecipeDto> = json.decodeFromString(responseText)
-                                    Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-                                    Log.i("!!!", responseText)
-                                    Log.i("!!!", "Категория: ${category.title}")
-                                    Log.i("!!!", "Рецептов в категории: ${recipes.size}")
-                                }
-                            } catch (e: Exception) {
-                                Log.i("!!!", "${e.message}")
-                            }
-                        }
+                        Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+                        Log.i("!!!", "Категория: ${category.title}")
+                        Log.i("!!!", "Рецептов в категории: ${recipes.size}")
+                    } catch (e: Exception) {
+                        Log.i("!!!", "${e.message}")
                     }
                 }
             } catch (e: Exception) {
@@ -86,10 +76,5 @@ class MainActivity : ComponentActivity() {
             deepLinkIntent = intent
         }
         setIntent(intent)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        threadPool.shutdown()
     }
 }
