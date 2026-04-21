067c756273ffc8656e549e70639da4403f616600
nasty8008 <nastya901296@gmail.com>
2026-04-22 01:02:51 +0400

    RCA-37: add constant for image base url

--- app/src/main/java/com/yourcompany/recipecomposeapp/core/utils/Constants.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/core/utils/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     const val ASSETS_URI_PREFIX = "file:///android_asset/"
     const val DEEP_LINK_SCHEME = "recipeapp"
     const val DEEP_LINK_BASE_URL = "https://recipes.androidsprint.ru"
+    const val IMAGES_BASE_URL = "https://recipes.androidsprint.ru/api/images/"
 
     const val KEY_RECIPE_OBJECT = "recipe"
     const val PARAM_RECIPE_ID = "recipeId"
