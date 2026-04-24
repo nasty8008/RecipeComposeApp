0a8accc534265b90d00c4c74354971485b66dc0c
nasty8008 <nastya901296@gmail.com>
2026-04-24 12:42:21 +0400

    RCA-38: add category dao interface

---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/database/dao/CategoryDao.kt
@@ -0,0 +1,12 @@
+package com.yourcompany.recipecomposeapp.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.yourcompany.recipecomposeapp.data.database.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+    @Query("SELECT * FROM categories ORDER BY name ASC")
+    fun getAllRecipes(): Flow<List<CategoryEntity>>
+}
