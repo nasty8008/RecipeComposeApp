ec09675abbdc8000fd53b01e7b2749263088226a
nasty8008 <nastya901296@gmail.com>
2026-04-27 16:53:49 +0400

    RCA-40: add insert function and queries to recipe dao

--- app/src/main/java/com/yourcompany/recipecomposeapp/data/database/dao/RecipeDao.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/database/dao/RecipeDao.kt
@@ -1,12 +1,26 @@
 package com.yourcompany.recipecomposeapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.yourcompany.recipecomposeapp.data.database.entity.RecipeEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdateRecipe(recipe: RecipeEntity)
+
     @Query("SELECT * FROM recipes")
     fun getAllRecipes(): Flow<List<RecipeEntity>>
+
+    @Query("SELECT * FROM recipes WHERE category_id = :categoryId")
+    fun getRecipesByCategory(categoryId: Int): Flow<List<RecipeEntity>>
+
+    @Query("SELECT * FROM recipes WHERE id = :recipeId")
+    fun getRecipeById(recipeId: Int): Flow<RecipeEntity>?
+
+    @Query("SELECT * FROM recipes WHERE id IN (:recipeIds)")
+    fun getRecipesByIds(recipeIds: List<Int>): Flow<List<RecipeEntity>>
 }
